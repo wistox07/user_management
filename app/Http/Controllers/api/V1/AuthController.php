41f1d992544bd9731501287d1fbf78584468bc35
wistox07 <fodelosrios@gmail.com>
2025-03-04 07:57:04 -0500

    Sucess response in loguin

--- app/Http/Controllers/api/V1/AuthController.php
+++ app/Http/Controllers/api/V1/AuthController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Session;
 use App\Models\User;
 use App\Models\UserSystemRole;
+use Dotenv\Repository\RepositoryInterface;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -46,11 +48,12 @@ class AuthController extends Controller
 
         try {
 
-            //DB::enableQueryLog();
+            DB::enableQueryLog();
+            DB::beginTransaction();
 
             //first devuelve un objeto UserSystemRole si hay datos y null , si no hay datos
             //get devuelve una coleccion de UserSystemRole si no hay datos lo que hace que sea true 
-            $userSystemRole = UserSystemRole::with(["user","role","system"])
+            $userSystemRole = UserSystemRole::with(["user.profile","role","system"])
             ->whereHas("user", function ($query) use ($userName) {
                 $query->where("username", $userName)
                 ->where("status", 1);
@@ -59,7 +62,12 @@ class AuthController extends Controller
             ->where("status", 1)
             ->first();
 
-            
+            $userSystemRoleSystemId = UserSystemRole::whereHas("user", function($query){
+                $query->where("username","sistema")
+                ->where("status", 1);
+            })->value("id");
+
+       
             if(!$userSystemRole){
                 return response()->json([
                     "error" => true,
@@ -68,9 +76,17 @@ class AuthController extends Controller
                 ], 400);
             }
 
-            $user = $userSystemRole->user;
+            if(!$userSystemRoleSystemId){
+                return response()->json([
+                    "error" => true,
+                    "message" => "No fue posible encontrar el usuario de sistema",
+                    "message_detail" => ""
+                ], 400);
+            }
 
-            if (!Hash::check($password, $user->password)) {
+            
+
+            if (!Hash::check($password, $userSystemRole->user->password)) {
                 return response()->json([
                     "error" => true,
                     "message" => "Credenciales incorrectas",
@@ -78,22 +94,27 @@ class AuthController extends Controller
                 ], 400);
             }
 
-            $token = JWTAuth::fromUser($user);
+            $token = JWTAuth::fromUser($userSystemRole->user);
 
 
             $session = new Session();
+            $session->user_system_role_id = $userSystemRole->id;
             $session->ip_adress = $ipAdress;
             $session->user_agent = $userAgent;
             $session->auth_token = $token;
             $session->status = 1;
-            $session->created_by;
+            $session->created_by = $userSystemRoleSystemId;
+            $session->save();
 
+            DB::commit();
 
             return response()->json([
                 "success" => true,
+                "data" => $userSystemRole,
                 "token" => $token
             ]);
         } catch (Throwable $ex) {
+            DB::rollBack();
             return response()->json([
                 "error" => true,
                 "message" => "Problema en el proceso de logueo",
--- app/Models/User.php
+++ app/Models/User.php
@@ -69,4 +69,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasMany(UserSystemRole::class, "user_id", "id");
     }
+
+    public function profile(){
+        return $this->hasOne(Profile::class, "user_id","id");
+    }
 }
